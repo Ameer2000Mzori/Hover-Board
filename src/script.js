bf624bff282aa1e6b3ec7a7fdbe638302a566771
Ameer <ameerapex496@gmail.com>
2023-12-09 19:58:46 +0100

    added added random num genorator / color obj of array

--- src/script.js
+++ src/script.js
@@ -1,11 +1,40 @@
 // selecting our elements
 var allSpans = document.querySelectorAll("span");
+// our array of colors
+var colorObject = [
+    {
+        color: "red",
+    },
+    {
+        color: "yellow",
+    },
+    {
+        color: "blue",
+    },
+    {
+        color: "green",
+    },
+    {
+        color: "white",
+    },
+];
 // functions
-var hoverEffect = function (e) {
+var startEffect = function (e) {
     var spanEl = e.target;
-    spanEl.classList.add("active");
+    setTimeout(function () {
+        spanEl.classList.add("active");
+        // spanEl.style.backgroundColor = "white";
+        var randNum = Math.floor(Math.random() * 5);
+        console.log(randNum);
+        spanEl.style.backgroundColor = "".concat(colorObject[randNum].color);
+    }, 10);
+    // added timeout after 1s effect should be removed!
+    setTimeout(function () {
+        // spanEl.classList.remove("active");
+        spanEl.style.backgroundColor = "black";
+    }, 1000);
 };
-// our eventlisnters
+// our eventlinster for each span
 allSpans.forEach(function (span) {
-    span.addEventListener("mouseenter", hoverEffect);
+    span.addEventListener("mouseenter", startEffect);
 });
--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,44 @@
 // selecting our elements
 let allSpans: any = document.querySelectorAll("span");
 
+// our array of colors
+let colorObject = [
+  {
+    color: "red",
+  },
+  {
+    color: "yellow",
+  },
+  {
+    color: "blue",
+  },
+  {
+    color: "green",
+  },
+  {
+    color: "white",
+  },
+];
+
 // functions
-const hoverEffect = (e) => {
-  let spanEl: any = e.target;
-  spanEl.classList.add("active");
+const startEffect = (e) => {
+  let spanEl: HTMLSpanElement = e.target;
+  setTimeout(() => {
+    spanEl.classList.add("active");
+    // spanEl.style.backgroundColor = "white";
+    let randNum = Math.floor(Math.random() * 5);
+    console.log(randNum);
+    spanEl.style.backgroundColor = `${colorObject[randNum].color}`;
+  }, 10);
+
+  // added timeout after 1s effect should be removed!
+  setTimeout(() => {
+    // spanEl.classList.remove("active");
+    spanEl.style.backgroundColor = `black`;
+  }, 1000);
 };
 
-// our eventlisnters
+// our eventlinster for each span
 allSpans.forEach((span: any) => {
-  span.addEventListener("mouseenter", hoverEffect);
+  span.addEventListener("mouseenter", startEffect);
 });
